55de694d772d92758a3df6f1ab74d4c243a61b8e
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-07-28 18:58:32 -0700

    typo

--- localSetup.sh
+++ localSetup.sh
@@ -1,5 +1,5 @@
 apt install software-properties-common -y
-apt-add-respository ppa:ansible/ansible -y
+apt-add-repository ppa:ansible/ansible -y
 apt update -y
 apt install ansible -y
 ansible-playbook -c local provision.yml
